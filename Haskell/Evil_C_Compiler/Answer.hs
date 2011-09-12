5ca95f5edceaf859459fe8a96eff1fa1f0e731e3
Kiwamu Okabe <kiwamu@debian.or.jp>
2011-09-12 23:15:08 +0900

    case

--- Haskell/Evil_C_Compiler/Answer.hs
+++ Haskell/Evil_C_Compiler/Answer.hs
@@ -1,5 +1,8 @@
 main :: IO ()
-main = putStr $ unlines $ ["Compiling...\nRunning..."] ++ f
+main = interact $ \s -> "Compiling...\nRunning...\n" ++ g s
+  where g a | "FizzBuzz()" `elem` words a = unlines f
+            | otherwise = ""
 
+-- fizzbuzz
 f :: [String]
 f = map i [1..100]where i n = let h = concat$concat[["Fizz"|g 3],["Buzz"|g 5]];g a = mod n a == 0;in last$h:[show n|h==""]
