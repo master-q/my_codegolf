8f39aa0cb70ca48cc4a330877171ec0209547caf
Kiwamu Okabe <kiwamu@debian.or.jp>
2011-09-13 00:26:38 +0900

    use dict

--- Haskell/Evil_C_Compiler/Answer.hs
+++ Haskell/Evil_C_Compiler/Answer.hs
@@ -1,27 +1,17 @@
-main :: IO ()
-main = interact $ \s -> "Compiling...\nRunning...\n" ++ g s
-  where g a | "FizzBuzz()" `elem` words a = unlines f
-            | "Fib()" `elem` words a = unlines k
-            | otherwise = unlines l
-
--- fizzbuzz
-f :: [String]
+main=interact(\s->"Compiling...\nRunning...\n"++(unlines $ g s))
+  where g a | "FizzBuzz()" `elem` words a = f
+            | "Fib()" `elem` words a = k
+            | otherwise = l
 f = map i [1..100]where i n = let h = concat$concat[["Fizz"|g 3],["Buzz"|g 5]];g a = mod n a == 0;in last$h:[show n|h==""]
-
--- fib
-k :: [String]
 k = map show (take 46 j)where j = 1:1:zipWith (+) j (tail j)
-
--- shinichiro
-l :: [String]
 l = map j [1..99]
   where
-    j n  = s (100 - n) ++ " of hamaji on the wall, " ++
-           s (100 - n) ++ " of hamaji.\n" ++
+    j n  = s (100 - n) ++ d!!3 ++ d!!4 ++ ", " ++
+           s (100 - n) ++ d!!3 ++ ".\n" ++
            m n
-    m 99 = "Go to the store and buy some more, " ++
-          s 99 ++ " of hamaji on the wall."
-    m n = "Take one down and pass it around, " ++
-          s (99 - n) ++ " of hamaji on the wall.\n"
-    s 1 = "1 shinichiro"
-    s n = show n ++ " shinichiroes"
+    m 99 = d!!2 ++ s 99 ++ d!!3 ++ d!!4 ++ "."
+    m n = d!!1 ++
+          s (99 - n) ++ d!!3 ++ d!!4 ++ ".\n"
+    s 1 = "1"++d!!0
+    s n = show n ++ d!!0 ++ "es"
+d = lines " shinichiro\nTake one down and pass it around, \nGo to the store and buy some more, \n of hamaji\n on the wall"
