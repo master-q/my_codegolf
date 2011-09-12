92942f73cb218315dd939881526e52a30d1b117a
Kiwamu Okabe <kiwamu@debian.or.jp>
2011-09-13 00:03:26 +0900

    test passed

--- Haskell/Evil_C_Compiler/Answer.hs
+++ Haskell/Evil_C_Compiler/Answer.hs
@@ -2,7 +2,7 @@ main :: IO ()
 main = interact $ \s -> "Compiling...\nRunning...\n" ++ g s
   where g a | "FizzBuzz()" `elem` words a = unlines f
             | "Fib()" `elem` words a = unlines k
-            | otherwise = ""
+            | otherwise = unlines l
 
 -- fizzbuzz
 f :: [String]
@@ -11,3 +11,17 @@ f = map i [1..100]where i n = let h = concat$concat[["Fizz"|g 3],["Buzz"|g 5]];g
 -- fib
 k :: [String]
 k = map show (take 46 j)where j = 1:1:zipWith (+) j (tail j)
+
+-- shinichiro
+l :: [String]
+l = map j [1..99]
+  where
+    j n  = s (100 - n) ++ " of hamaji on the wall, " ++
+           s (100 - n) ++ " of hamaji.\n" ++
+           m n
+    m 99 = "Go to the store and buy some more, " ++
+          s 99 ++ " of hamaji on the wall."
+    m n = "Take one down and pass it around, " ++
+          s (99 - n) ++ " of hamaji on the wall.\n"
+    s 1 = "1 shinichiro"
+    s n = show n ++ " shinichiroes"
