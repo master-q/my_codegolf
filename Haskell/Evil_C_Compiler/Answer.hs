6574f6d02890e8d4427f04c4d6ae149e29ee3798
Kiwamu Okabe <kiwamu@debian.or.jp>
2011-09-13 00:52:37 +0900

    now

--- Haskell/Evil_C_Compiler/Answer.hs
+++ Haskell/Evil_C_Compiler/Answer.hs
@@ -1,16 +1,14 @@
-main=interact(\s->"Compiling...\nRunning...\n"++(unlines $ g s))
+main=interact(\z->"Compiling...\nRunning...\n"++unlines (g z))
   where g a | "FizzBuzz()" `elem` words a = f
             | "Fib()" `elem` words a = k
             | otherwise = l
 f = map i [1..100]where i n = let h = concat$concat[["Fizz"|g 3],["Buzz"|g 5]];g a = mod n a == 0;in last$h:[show n|h==""]
-k = map show (take 46 j)where j = 1:1:zipWith (+) j (tail j)
+k = map show (take 46 t)
+t = 1:1:zipWith (+) t (tail t)
 l = map j $ reverse [1..99]
-  where
-    j n  = s n ++ d!!3 ++ d!!4 ++ ", " ++
-           s n ++ d!!3 ++ ".\n" ++
-           m n
-    m 1 = d!!2 ++ s 99 ++ d!!3 ++ d!!4 ++ "."
-    m n = d!!1 ++ s (n - 1) ++ d!!3 ++ d!!4 ++ ".\n"
-    s 1 = "1"++d!!0
-    s n = show n ++ d!!0 ++ "es"
+j n  = s n++d!!3++d!!4++", "++s n++d!!3++".\n"++m n
+m 1 = d!!2++s 99++d!!3++d!!4++"."
+m n = d!!1++s (n - 1)++d!!3++d!!4++".\n"
+s 1 = '1':d!!0
+s n = show n++d!!0++"es"
 d = lines " shinichiro\nTake one down and pass it around, \nGo to the store and buy some more, \n of hamaji\n on the wall"
