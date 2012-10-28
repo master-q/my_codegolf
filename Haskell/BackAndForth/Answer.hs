0e4e65b0f44707518dbb120f0e77376eae775e95
Kiwamu Okabe <kiwamu@debian.or.jp>
2012-10-29 01:09:24 +0900

    227 char

--- Haskell/BackAndForth/Answer.hs
+++ Haskell/BackAndForth/Answer.hs
@@ -1,6 +1,6 @@
-adjust _ _ [] = ""
-adjust r t (x:xs) = replicate (t - d) ' ' ++ x ++ "\n" ++ adjust ((r+1)`mod`2) t' xs
-  where w = if r == 0 then (+) else (-)
-        d = if r == 0 then 0 else length x
-        t' = w t (length x)
-main=interact(adjust 0 0.zipWith($)(cycle[id,reverse]).words)
+j _ _ []=""
+j r t (x:xs)=replicate (t - d) ' ' ++ x ++ "\n" ++ j ((r+1)`mod`2) u xs
+  where w=last$(-):[(+)|r==0]
+        d=last$length x:[0|r==0]
+        u=w t$length x
+main=interact(j 0 0.zipWith($)(cycle[id,reverse]).words)
