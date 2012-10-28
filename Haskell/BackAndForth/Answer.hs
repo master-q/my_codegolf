c742fdbbd6a9a17c0a91764ee213befa2c86092b
Kiwamu Okabe <kiwamu@debian.or.jp>
2012-10-29 01:36:16 +0900

    178 char

--- Haskell/BackAndForth/Answer.hs
+++ Haskell/BackAndForth/Answer.hs
@@ -1,4 +1,4 @@
 l=length
 j _ _[]=""
-j r t(x:s)=([1..t-d]>>" ")++x++"\n"++j(mod(r+1)2) u s where d=last$l x:[0|r==0];u=last$(t-l x):[t+l x|r==0]
+j r t(x:s)=([1..t-d]>>" ")++x++"\n"++j(mod(r+1)2) u s where(d,u)=last$(l x,t-l x):[(0,t+l x)|r==0]
 main=interact(j 0 0.zipWith($)(cycle[id,reverse]).words)
